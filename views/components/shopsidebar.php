5751ec34eabb6e7138278c3e6a5beb45ed50719b
rizzo <mmaririchard11@gmail.com>
2019-12-06 11:09:49 +0300

    new files introduced
    a template was added

---
+++ views/components/shopsidebar.php
@@ -0,0 +1,21 @@
+<div class="shop_sidebar_area">
+
+<!-- ##### Single Widget ##### -->
+<div class="widget catagory mb-50">
+    <!-- Widget Title -->
+    <h6 class="widget-title mb-30">Catagories</h6>
+
+    <!--  Catagories  -->
+    <div class="catagories-menu">
+        <ul>
+            <li class="active"><a href="#">Chairs</a></li>
+            <li><a href="#">Beds</a></li>
+            <li><a href="#">Accesories</a></li>
+            <li><a href="#">Furniture</a></li>
+            <li><a href="#">Home Deco</a></li>
+            <li><a href="#">Dressings</a></li>
+            <li><a href="#">Tables</a></li>
+        </ul>
+    </div>
+</div>
+</div>
